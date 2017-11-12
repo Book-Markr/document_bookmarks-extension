7c97b785155cdbef080b7268e2aa683439d5775f
Nityesh Agarwal <nityeshagarwal@gmail.com>
2017-11-13 01:02:27 +0530

    First implementation
    The extension can:
    * fetch a folder from inside the Mobile bookmarks folder irrespective
    of its capitalised nature
    * access the URL of the currently open tab using Tabs permission in the
    manifest
    * store the URL inside the fetched folder with its title and a user
    provided comment

--- popup.js
+++ popup.js
@@ -1,11 +1,32 @@
-$(function()){
+$(function(){
+
 	$('#add').click(function(){
-		var comment= $('#commment').val();
-		var path= $('#folderPath').val();
-		chrome.tabs.getCurrent(function(tab){
-			chrome.bookmarks.create({'parentId': bookmarkBar.id,
-									 'title': comment,
-									 'url': tab.url});
+		chrome.bookmarks.getTree(function(FolderNodes){
+			var i, flag= 0;
+			for(i= 0; i< (FolderNodes[0].children[2].children.length); ++i){
+				/*convert both strings to upper case to remove petty match conflicts*/
+				if((FolderNodes[0].children[2].children[i].title.toUpperCase()) ==
+					$('#folderPath').val().toUpperCase()){
+						flag= 1;
+						break;
+				}
+			}
+			if(!flag)
+				$('#test_eg').text('Folder not found');
+			
+			else{
+				var ExtensionBookmarksNode= FolderNodes[0].children[2].children[i];
+				var currentURL;
+				chrome.tabs.query({active:true, currentWindow:true}, function(currentTabs){
+					currentURL= currentTabs[0].url;
+					//$('#test_eg').text(currentURL);
+					chrome.bookmarks.create({'parentId': ExtensionBookmarksNode.id,
+											 'title': currentTabs[0].title + 
+											 ' || ' +  $('#comment').val(),
+											 'url': currentURL});
+
+				});
+			}
 		});
-	};
-}
+	});
+});
