38fae8b65ceaa3124974c7aef3fffba3ba89ddd6
Nityesh Agarwal <nityeshagarwal@gmail.com>
2018-01-17 12:10:02 +0530

    popup.js: added function `addBookmark()`
    Started modularising the source code by creating a new function-
    `addBookmark()`. Closes #7

--- popup.js
+++ popup.js
@@ -1,34 +1,43 @@
 $(function(){
-
 	$('#add').click(function(){
-		chrome.bookmarks.getTree(function(FolderNodes){
-			var i, flag= 0;
-			for(i= 0; i< (FolderNodes[0].children[2].children.length); ++i){
-				/*convert both strings to upper case to remove petty match conflicts*/
-				if((FolderNodes[0].children[2].children[i].title.toUpperCase()) ==
-					$('#folderPath').val().toUpperCase()){
-						flag= 1;
-						break;
-				}
+		destination= $('#folderPath').val().toUpperCase();
+		addBookmark(destination, function(errorCode){
+			if(errorCode==0){  //success
+				$('#result').text(destination);
 			}
-			if(!flag)
-				$('#test_eg').text('Folder not found');
-			
-			else{
-				var ExtensionBookmarksNode= FolderNodes[0].children[2].children[i];
-				var currentURL;
-
-				$('#test_eg').text(ExtensionBookmarksNode.title);
-				chrome.tabs.query({active:true, currentWindow:true}, function(currentTabs){
-					currentURL= currentTabs[0].url;
-					//$('#test_eg').text(currentURL);
-					chrome.bookmarks.create({'parentId': ExtensionBookmarksNode.id,
-											 'title': currentTabs[0].title + 
-											 ' || ' +  $('#comment').val(),
-											 'url': currentURL});
-
-				});
+			else if(errorCode==1){
+				$('#result').text("Folder not found");
 			}
 		});
 	});
 });
+
+/*function that accepts a 'destination' folder name and a callback function `respond`*/
+function addBookmark(destination, respond){
+	chrome.bookmarks.getTree(function(FolderNodes){
+		var i, failure= 1;
+		for(i= 0; i< (FolderNodes[0].children[2].children.length); ++i){
+		//convert both strings to upper case to remove petty match conflicts
+			if((FolderNodes[0].children[2].children[i].title.toUpperCase()) == destination){
+				failure= 0;
+				break;
+			}
+		}
+		if(failure==1){
+			respond(1);
+		}
+
+		else{
+			var ExtensionBookmarksNode= FolderNodes[0].children[2].children[i];
+			var currentURL;
+			chrome.tabs.query({active:true, currentWindow:true}, function(currentTabs){
+				currentURL= currentTabs[0].url;
+				chrome.bookmarks.create({'parentId': ExtensionBookmarksNode.id,
+					'title': currentTabs[0].title + 
+					' || ' +  $('#comment').val(),
+					'url': currentURL});
+			});
+			respond(0);
+		}
+	});
+}
