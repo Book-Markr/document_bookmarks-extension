2f17f20f189ff22d098979c130516706b086fc5f
Nityesh Agarwal <nityeshagarwal@gmail.com>
2018-01-17 15:39:40 +0530

    popup.js: added feature to allow folder paths
    Extension can now locate folder located at arbitrary depths if the
    complete folder path, starting from 'Mobile Bookmarks', is specified
    Closes #2.

--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
 $(function(){
 	$('#add').click(function(){
-		destination= $('#folderPath').val().toUpperCase();
+		var input= $('#folderPath').val().toUpperCase();
+		var destination= input.split("/");
 		addBookmark(destination, function(errorCode){
 			if(errorCode==0){  //success
-				$('#result').text(destination);
+				$('#result').text(destination[destination.length-1]);
 			}
 			else if(errorCode==1){
 				$('#result').text("Folder not found");
@@ -12,23 +13,40 @@ $(function(){
 	});
 });
 
-/*function that accepts a 'destination' folder name and a callback function `respond`*/
 function addBookmark(destination, respond){
+/*addBookmark(): accepts a 'destination' folder name and a callback 
+				 function `respond`
+				 Adds the current tab URL to the `destination` folder,
+				 if possible. Otherwise, calls the callback `respond()`
+				 with an appropriate error code*/
+
 	chrome.bookmarks.getTree(function(FolderNodes){
-		var i, failure= 1;
-		for(i= 0; i< (FolderNodes[0].children[2].children.length); ++i){
-		//convert both strings to upper case to remove petty match conflicts
-			if((FolderNodes[0].children[2].children[i].title.toUpperCase()) == destination){
-				failure= 0;
+		var i, failure= 1; 
+		var base= FolderNodes[0].children[2];  //'Mobile Bookmarks' folder
+		var htFromDest= destination.length;
+
+		while(htFromDest!=0){
+			failure= 1;
+			for(i= 0; i< (base.children.length); ++i){
+			//convert both strings to upper case to remove petty match conflicts
+				if((base.children[i].title.toUpperCase()) == 
+					destination[destination.length-htFromDest].toUpperCase()){
+					failure= 0;
+					break;
+				}
+			}
+			if(failure==1){
+				respond(1);
 				break;
 			}
-		}
-		if(failure==1){
-			respond(1);
+			else{
+				base= base.children[i];
+				--htFromDest;
+			}
 		}
 
-		else{
-			var ExtensionBookmarksNode= FolderNodes[0].children[2].children[i];
+		if(failure==0){
+			var ExtensionBookmarksNode= base;
 			var currentURL;
 			chrome.tabs.query({active:true, currentWindow:true}, function(currentTabs){
 				currentURL= currentTabs[0].url;
