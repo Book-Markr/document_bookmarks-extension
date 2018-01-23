d4eb271d092eebbde1bf0deb002d299ebdfcd1ab
Nityesh Agarwal <nityeshagarwal@gmail.com>
2018-01-24 01:44:50 +0530

    popup.js: modified to allow multiple comments
    If a bookmark that is already present in the 'Mobile Bookmarks' folder
    is added, the extension will now update the title by adding the new
    comment (if present) instead of creating a new entry for the same URL.
    Closes #8.

--- popup.js
+++ popup.js
@@ -4,12 +4,17 @@ $(function(){
 	$('#add').click(function(){
 		var input= $('#folderPath').val().toUpperCase();
 		var destination= input.split("/");
-		addBookmark(destination, function(errorCode){
-			if(errorCode==0){  //success
+		addBookmark(destination, function(respondCode){
+			if(respondCode==0){  //success
 				$('#result').text(destination[destination.length-1]);
 			}
-			else if(errorCode==1){
+			else if(respondCode==1){
 				$('#result').text("Folder not found");
+			}
+			else if(respondCode==2){
+				responseMsg= destination[destination.length-1] + 
+							 "/ Comment added";
+				$('#result').text(responseMsg);
 			}
 		});
 	});
@@ -47,17 +52,37 @@ function addBookmark(destination, respond){
 			}
 		}
 
+		//the desired folder exists
 		if(failure==0){
-			var ExtensionBookmarksNode= base;
+			var theFolderNode= base;
 			var currentURL;
 			chrome.tabs.query({active:true, currentWindow:true}, function(currentTabs){
 				currentURL= currentTabs[0].url;
-				chrome.bookmarks.create({'parentId': ExtensionBookmarksNode.id,
-					'title': currentTabs[0].title + 
-					' || ' +  $('#comment').val(),
-					'url': currentURL});
+				var idx= alreadyExists(currentURL, theFolderNode);
+				if(idx >= 0){
+					var newTitle= theFolderNode.children[idx].title + " || " + $('#comment').val();
+					chrome.bookmarks.update(theFolderNode.children[idx].id, {'title': newTitle});
+					respond(2);
+				}
+				else{
+					chrome.bookmarks.create({'parentId': theFolderNode.id,
+						'title': currentTabs[0].title + 
+						' || ' +  $('#comment').val(),
+						'url': currentURL});
+					respond(0);
+				}
 			});
-			respond(0);
 		}
 	});
+}
+
+function alreadyExists(URL, folder){
+	var i;
+	for(i=0; i< folder.children.length; ++i){
+		if(folder.children[i].url == URL){
+			//$('#result').text(i);
+			return i;
+		}
+	}
+	return -1;
 }
