4b472ef6a1729b7a2b9b2c9139c619670845b12c
Nityesh Agarwal <nityeshagarwal@gmail.com>
2018-06-20 12:14:40 +0530

    markr.js: Allow formatting a webpage
    Added the capability to bolden, italicise or highlight the selected text
    on a webpage using this (content) script.
    
    Closes #26.

--- markr.js
+++ markr.js
@@ -3,10 +3,29 @@
  * (toggle the bold/italics/highlight setting)
  */
 
+var formatted = [];
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+
 	if(request.ping){ 
 		sendResponse({pong: true}); 
 		return; 
 	}
-	console.log("Text " + (request.currentState ? "" : "un") + request.changeParameter + " working");
+	
+	var wrapper = document.createElement("span");
+	if(request.changeParameter == 'highlight'){
+		wrapper.style.background = "yellow";
+	}
+	else if(request.changeParameter == 'bold'){
+		wrapper.style.fontWeight = "bold";
+	}
+	else if(request.changeParameter == 'italics'){
+		wrapper.style.fontStyle = "italic";
+	}
+
+	/* Follow this to see the problems with the following approach:
+	 * https://stackoverflow.com/a/29895300/7082018 (in the comments)*/
+	var selectedRange = window.getSelection().getRangeAt(0);
+	wrapper.appendChild(selectedRange.extractContents());
+	selectedRange.insertNode(wrapper);
 });
